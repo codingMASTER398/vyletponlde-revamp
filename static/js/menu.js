0dc33ccbfad6f0905e71db77bb1bd9013879c1f9
codingMASTER398 <lachienoble10@outlook.com>
2025-09-12 17:10:45 +1000

    i can't think of a commit message for this

--- static/js/menu.js
+++ static/js/menu.js
@@ -3,6 +3,8 @@ function openMenu(id) {
     if (c.classList.contains(id)) c.style.display = "";
     else c.style.display = "none";
   });
+
+  window.location.hash = id;
 }
 
 [...document.querySelectorAll(`div[data-menu-id]`)].forEach((c) => {
@@ -10,3 +12,7 @@ function openMenu(id) {
     openMenu(c.getAttribute(`data-menu-id`));
   });
 });
+
+if(["#menuNormal", "#menuEasy", "#menuSpecial", "#menuAbout", "#menuTracks", "#menuCat"].includes(window.location.hash)) {
+  openMenu(window.location.hash.replace("#", ""))
+}
